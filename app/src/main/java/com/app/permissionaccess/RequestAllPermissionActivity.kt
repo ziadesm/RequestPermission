aa5c1ec61676a5353377436b5bd13ec600dd3e09
Ziad <zi.eus@hotmail.com>
2022-05-18 15:28:59 +0200

    main commit

--- app/src/main/java/com/app/permissionaccess/RequestAllPermissionActivity.kt
+++ app/src/main/java/com/app/permissionaccess/RequestAllPermissionActivity.kt
@@ -8,22 +8,12 @@ class RequestAllPermissionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_all_permission)
-
-
-
-
-
-
+        
         RequestAccessPermission()
             .create(this)
             .permissions(arrayListOf(Manifest.permission.ACCESS_COARSE_LOCATION))
             .isRequired(true)
             .isSensitive(true)
             .build()
-
-
-
-
-
     }
 }
